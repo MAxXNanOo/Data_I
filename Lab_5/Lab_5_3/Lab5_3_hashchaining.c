fc03055c857680263d74a29868b7cfbdd91af830
MAxXNanOo <maxxnanaoo@gmail.com>
2025-07-31 14:40:47 +0700

    Data_I

--- Lab_5/Lab_5_3/Lab5_3_hashchaining.c
+++ Lab_5/Lab_5_3/Lab5_3_hashchaining.c
@@ -1,115 +1,111 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-struct  contact
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+struct contact
 {
   char name[5];
   char number[10];
   struct contact *next;
 };
 
-struct contact* createNode(char name[], char number[])
+struct contact *createNode(char name[], char number[])
 {
-    struct contact *new_node;
-    new_node= (struct contact*) malloc(sizeof(struct contact));
-    //assign value for newnode
-    strcpy(new_node->name, name);
-    strcpy(new_node->number, number);
-    new_node->next=NULL;
+  struct contact *new_node;
+  new_node = (struct contact *)malloc(sizeof(struct contact));
+  // assign value for newnode
+  strcpy(new_node->name, name);
+  strcpy(new_node->number, number);
+  new_node->next = NULL;
 
-    return new_node;
+  return new_node;
 }
 
 int hash2(char key[], int n)
 {
-      int i,j=0;
-      long long int hashVal = 0;
-      for (i=strlen(key)-1; i>=0; i--)
-      {
-        hashVal += key[i] *pow(32.0,j);
-        j++;
-
-      }
-    return hashVal%n;
+  int i, j = 0;
+  long long int hashVal = 0;
+  for (i = strlen(key) - 1; i >= 0; i--)
+  {
+    hashVal += key[i] * pow(32.0, j);
+    j++;
+  }
+  return hashVal % n;
 }
 
-
-void searchList(struct contact* head, char name[])
+void searchList(struct contact *head, char name[])
 {
-  //fill-in your code for searching value in hash table
-  while(true)
+  // fill-in your code for searching value in hash table
+  if(head) printf("%s 's number is %s\n",head->name,head->number);
+  else printf("Not found %s in the contact list\n",name);
 
 }
 
 int main()
 {
-   int i,n,m=13,k;
-   struct contact *hash_table[m];
-   struct contact *new_node;
+  int i, n, m = 13, k;
+  struct contact *hash_table[m];
+  struct contact *new_node;
 
-   struct contact *ptr=NULL,*preptr=NULL;
+  struct contact *ptr = NULL, *preptr = NULL;
 
-   char name[6];
-   char number[11];
+  char name[6];
+  char number[11];
 
-   //initialize a chained hash table
-   for(i=0;i<m;i++)
-     hash_table[i]=NULL;
+  // initialize a chained hash table
+  for (i = 0; i < m; i++)
+    hash_table[i] = NULL;
 
-   do
-   {
-     printf("=== Contact List ===\n");
-     printf("1. Add new contact\n");
-     printf("2. Remove contact\n");
-     printf("3. Search contact\n");
-     printf("4. Exit\n");
-     printf("Enter your choice:");
-     scanf("%d",&n);
+  do
+  {
+    printf("\n=== Contact List ===\n");
+    printf("1. Add new contact\n");
+    printf("2. Remove contact\n");
+    printf("3. Search contact\n");
+    printf("4. Exit\n");
+    printf("Enter your choice:");
+    scanf("%d", &n);
 
-     if(n==1)
-     {
-         printf("Enter name:");
-         scanf("%s",name);
-         printf("Enter phone number:");
-         scanf("%s",number);
+    if (n == 1)
+    {
+      printf("Enter name:");
+      scanf("%s", name);
+      printf("Enter phone number:");
+      scanf("%s", number);
 
-         //call hash function for string
-         new_node = createNode(name, number);
+      // call hash function for string
+      k = hash2(name, m);
 
-         printf("key:%s has hashkey=%d\n",name,k);
+      printf("key:%s has hashkey=%d\n", name, k);
 
-         //insert node into Linkedlist at head
-        
+      // insert node into Linkedlist at head
+      new_node = createNode(name, number);
+      new_node->next = hash_table[k];
+      hash_table[k] = new_node;
+    }
+    else if (n == 2)
+    {
+      printf("Enter name:");
+      scanf("%s", name);
 
+      // fill-in your code for deleting node with the specific name
+      k = hash2(name, m);
+      hash_table[k] = NULL;
+    }
+    else if (n == 3) // search
+    {
+      printf("Enter name:");
+      scanf("%s", name);
 
-     }
-     else if(n==2)
-     {
-        printf("Enter name:");
-        scanf("%s",name);
+      // call hash function for string
+      k = hash2(name, m);
 
-        //fill-in your code for deleting node with the specific name
+      searchList(hash_table[k], name);
+    }
 
-     }
-     else if(n==3)  //search
-     {
-         printf("Enter name:");
-         scanf("%s",name);
+  } while (n != 4);
 
-         //call hash function for string
-
-
-         searchList(hash_table[k],name);
-
-     }
-
-   }while(n!=4);
-
-
-
-   //free memory
-
+  // free memory
 
   return 0;
 }
