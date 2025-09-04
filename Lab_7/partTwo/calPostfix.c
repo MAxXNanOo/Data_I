2a98f381756964138ff18fe32ffa11e684de051c
MAxXNanOo <MAxXNanOo@gmail.com>
2025-09-04 13:35:11 +0700

    Update calPostfix.c

--- Lab_7/partTwo/calPostfix.c
+++ Lab_7/partTwo/calPostfix.c
@@ -2,48 +2,34 @@
 
 int main()
 {
-    char arr[50], sym[50];
-    int numTop = -1, symTop = -1, num[50], size=0;
+    char arr[50];
+    int numTop = -1, num[50];
 
     scanf("%s", &arr);
 
     for(int i=0 ; arr[i]!='\0' ; i++){
         if(arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/' || arr[i] == '^'){
-            
+            switch(arr[i]){
+                case '+':
+                    num[numTop-1] = num[numTop-1] + num[numTop ];
+                break;
+
+                case '-':
+                    num[numTop-1] = num[numTop-1] - num[numTop ];
+                break;
+
+                case '*':
+                    num[numTop-1] = num[numTop-1] * num[numTop ];
+                break;
+
+                case '/':
+                    num[numTop-1] = num[numTop-1] / num[numTop ];
+                break;
+            }
+            numTop--;
         }
         else{
             num[++numTop] = (int)arr[i]-48;
-        }
-
-        size++;
-    }
-
-    for(int i=size-1 ; i>=0 ; i--){
-        if(arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/' || arr[i] == '^'){
-            sym[++symTop] = arr[i];
-        }
-        else{
-            
-        }
-    }
-
-    for( ; numTop != 0 ; numTop--, symTop--){
-        switch(sym[symTop]){
-            case '+':
-                num[numTop-1] = num[numTop] + num[numTop - 1];
-            break;
-
-            case '-':
-                num[numTop-1] = num[numTop] - num[numTop - 1];
-            break;
-
-            case '*':
-                num[numTop-1] = num[numTop] * num[numTop - 1];
-            break;
-
-            case '/':
-                num[numTop-1] = num[numTop] / num[numTop - 1];
-            break;
         }
     }
 
@@ -53,3 +39,59 @@ int main()
     //     printf("%c",sym[symTop--]);
     // }
 }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     char arr[50], sym[50];
+//     int numTop = -1, symTop = -1, num[50], size=0;
+
+//     scanf("%s", &arr);
+
+//     for(int i=0 ; arr[i]!='\0' ; i++){
+//         if(arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/' || arr[i] == '^'){
+            
+//         }
+//         else{
+//             num[++numTop] = (int)arr[i]-48;
+//         }
+
+//         size++;
+//     }
+
+//     for(int i=size-1 ; i>=0 ; i--){
+//         if(arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/' || arr[i] == '^'){
+//             sym[++symTop] = arr[i];
+//         }
+//         else{
+            
+//         }
+//     }
+
+//     for( ; numTop != 0 ; numTop--, symTop--){
+//         switch(sym[symTop]){
+//             case '+':
+//                 num[numTop-1] = num[numTop] + num[numTop - 1];
+//             break;
+
+//             case '-':
+//                 num[numTop-1] = num[numTop] - num[numTop - 1];
+//             break;
+
+//             case '*':
+//                 num[numTop-1] = num[numTop] * num[numTop - 1];
+//             break;
+
+//             case '/':
+//                 num[numTop-1] = num[numTop] / num[numTop - 1];
+//             break;
+//         }
+//     }
+
+//     printf("\n%d",num[0]);
+
+//     // while(symTop != -1){
+//     //     printf("%c",sym[symTop--]);
+//     // }
+// }
