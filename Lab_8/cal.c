c02b78cad49b6b591658e71ce7c6c5c2d21a6b14
MAxXNanOo <maxxnanoo@gmail.com>
2025-09-16 00:32:44 +0700

    Data_I

---
+++ Lab_8/cal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Treenode
+{
+    char data;
+    struct Treenode *leftChild;
+    struct Treenode *rightChild;
+};
+
+struct Treenode *Root = NULL;
+
+int main()
+{
+    char input[50], ch;
+    scanf("%s",&input);
+
+     
+    for(int i=1 ; input[i] != '\0' ; i++){
+        ch = input[i];
+        if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+
+        }
+    }
+}
