9b3f9845dcfe9676de63117c0e5a17564057a2f3
MAxXNanOo <maxxnanoo@gmail.com>
2025-09-19 22:12:11 +0700

    Data_I

--- Lab_8/cal.c
+++ Lab_8/cal.c
@@ -15,7 +15,7 @@ int main()
     char input[50], ch;
     scanf("%s",&input);
 
-     
+      
     for(int i=1 ; input[i] != '\0' ; i++){
         ch = input[i];
         if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
