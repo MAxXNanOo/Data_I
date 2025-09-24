cb4293ac1a9e001ec177f70bbf8ab8b826b22fe8
MAxXNanOo <maxxnanoo@gmail.com>
2025-09-25 00:17:44 +0700

    Dataj_I

---
+++ Lab_10/AVLv2.c
@@ -0,0 +1,84 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <math.h>
+
+
+struct Treenode { 
+ int data,ht; 
+ struct Treenode *leftChild,*rightChild; 
+ struct Treenode *mother; 
+};
+
+struct Treenode *Root; 
+
+struct Treenode* createNode(int data)
+{
+    struct Treenode *new_node;
+    new_node= (struct Treenode*) malloc(sizeof(struct Treenode));
+    new_node->data =data;
+    new_node->ht = 0;
+    new_node->leftChild =NULL;
+    new_node->rightChild=NULL;
+    new_node->mother=NULL;
+    return new_node;
+}
+
+void Tree_insert( int data )
+{
+  struct Treenode *ptr = Root, *tmp=NULL;
+
+
+
+  while(1){
+    if(!Root){
+      Root = createNode(data);
+      ptr->ht = 0;
+      break;
+    }
+        
+    if(data <= ptr->data){
+      if(!ptr->leftChild){
+        ptr->ht++;
+        ptr->leftChild = createNode(data);
+        ptr->ht=0;
+        tmp = ptr->leftChild;
+        tmp->mother = ptr;
+        break;
+      }
+      ptr->ht++;
+      ptr = ptr->leftChild;
+    }
+    else{
+      if(!ptr->rightChild){
+        ptr->ht--;
+        ptr->rightChild = createNode(data);
+        ptr->ht=0;
+        tmp = ptr->rightChild;
+        tmp->mother = ptr;
+        break;
+      }
+      ptr->ht--;
+      ptr = ptr->rightChild;
+    }
+  }
+
+}
+
+void inorder(struct Treenode *r){ 
+//modify inorder() from Lab9 to print balance factor
+    if(r){
+        inorder(r->leftChild);
+        printf("%d(BF=%d)", r->data,r->ht);
+        inorder(r->rightChild);
+    }
+}
+
+int main()  { 
+  int a[]={17,3,19,1,6,18,22,4,25};    
+  int i,size; 
+  size=sizeof(a)/sizeof(a[0]); 
+  for(i=0;i<size;i++) 
+     Tree_insert (a[i]); 
+  inorder(Root);    
+  return 0;
+}
