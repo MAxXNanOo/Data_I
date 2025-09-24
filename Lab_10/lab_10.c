555fc64f10e8d1f33fee9f45107c748beb8860fa
MAxXNanOo <maxxnanaoo@gmail.com>
2025-09-25 00:33:09 +0700

    Data_I

--- Lab_10/lab_10.c
+++ Lab_10/lab_10.c
@@ -9,7 +9,15 @@ struct Treenode {
  struct Treenode *mother; 
 };
 
-struct Treenode *Root; 
+
+struct Treenode *Root,*S[100];
+
+int height(struct Treenode *ptr)
+{
+    if(ptr!=NULL)
+        return 1;
+    return 0;
+} 
 
 struct Treenode* createNode(int data)
 {
@@ -19,56 +27,55 @@ struct Treenode* createNode(int data)
     new_node->leftChild =NULL;
     new_node->rightChild=NULL;
     new_node->mother=NULL;
+    new_node->ht=1;
     return new_node;
 }
 
 void Tree_insert( int data )
 {
-  struct Treenode *ptr = Root, *tmp=NULL;
-
-
-
-  while(1){
-    
-    if(!Root){
-      Root = createNode(data);
-      Root->ht = 0;
-      break;
+  struct Treenode *ptr = Root, *tmp=NULL,*x=createNode(data);
+  int top=-1;
+   top++;
+   //printf("%d",top);
+   S[top]=ptr;
+    if(Root==NULL){
+      Root = x;
     }
-    
+    else{
     if(data <= ptr->data){
       if(!ptr->leftChild){
-        ptr->ht++;
-        ptr->leftChild = createNode(data);
-        ptr->leftChild->ht=0;
+        ptr->leftChild = x;
         tmp = ptr->leftChild;
         tmp->mother = ptr;
-        break;
       }
-      ptr->ht++;
       ptr = ptr->leftChild;
     }
     else{
       if(!ptr->rightChild){
-        ptr->ht--;
-        ptr->rightChild = createNode(data);
-        ptr->rightChild->ht=0;
+        ptr->rightChild = x;
         tmp = ptr->rightChild;
         tmp->mother = ptr;
-        break;
+        //break;
       }
-      ptr->ht--;
       ptr = ptr->rightChild;
     }
   }
+  while(top!=-1){
+    printf("%d",top);
+    struct Treenode *t=S[top];
+    t->ht=height(t)+1;
+    top--;
+  }
 
 }
+
+
 
 void inorder(struct Treenode *r){ 
 //modify inorder() from Lab9 to print balance factor
     if(r){
         inorder(r->leftChild);
-        printf("%d(BF=%d)", r->data,r->ht);
+        printf("%d(BF=%d)", r->data, r->ht);
         inorder(r->rightChild);
     }
 }
@@ -77,9 +84,8 @@ int main()  {
   int a[]={17,3,19,1,6,18,22,4,25};    
   int i,size; 
   size=sizeof(a)/sizeof(a[0]); 
-  for(i=0;i<size;i++) {
-    Tree_insert (a[i]);
-  }
+  for(i=0;i<size;i++) 
+     Tree_insert (a[i]); 
   inorder(Root);    
   return 0;
 }
